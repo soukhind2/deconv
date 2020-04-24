d6f517faf88797516031fdbaf9e56392fd519e91
soukhind2 <skndas@ucdavis.edu>
2020-04-24 05:46:11 -0700

    Overall working, PCA conflict,.gitignore reedited

--- test.py
+++ test.py
@@ -25,67 +25,53 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import importlib as imp
 import matplotlib.pyplot as plt
-pca = PCA(n_components = 10)
-p1 = np.zeros((19,19))
-p2 = np.zeros((19,19))
-a1 = np.zeros((19,19))
-c1 = np.zeros((19,19))
-c2 = np.zeros((19,19))
-c3 = np.zeros((19,19))
+pca = PCA(n_components = 10,whiten = True)
+p1 = np.zeros((18,18))
+p2 = np.zeros((18,18))
+a1 = np.zeros((18,18))
+c1 = np.zeros((18,18))
+c2 = np.zeros((18,18))
+c3 = np.zeros((18,18))
 
-for lisi in range(1,19):
-    for uisi in range(1,19):
+k = 0
+
+for lisi in np.arange(1,19,1):
+    l = 0
+    for uisi in np.arange(1,19,1):
         if lisi > uisi:
+            l+=1
             continue
         d = design.expdesign(1, lisi, uisi, 0.1, 100, [102], lv)
         data = d.tcourse()
         e = design.expanalyse(data, np.array([1]), expdesign = d)
-        p1[lisi-1,uisi-1] = e.calc_Fd()
-        p2[lisi-1,uisi-1] = e.calc_Fe(ncond = 1)
+        p1[k,l] = e.calc_Fd()
+        p2[k,l] = e.calc_Fe(ncond = 1)
         temp = e.roi/np.mean(e.roi)*100-100
-        a1[lisi-1,uisi-1] = trapz(temp,dx =1)
-        out = avgHRF(e.expdesign.onsets_A, temp, e.expdesign.loadvolume.tr)
+        a1[k,l] = trapz(temp,dx =1)
+        out = avgHRF(e.expdesign.onsets_A, temp, e.expdesign.loadvolume.tr) 
+        temp = temp.reshape(1,-1)
         pca.fit(out)
-        c1[lisi-1,uisi-1] = pca.explained_variance_[0]
-        c2[lisi-1,uisi-1] = pca.explained_variance_[1]
-        c3[lisi-1,uisi-1] = pca.explained_variance_[2]
-        if uisi == 14 and lisi == 2:
-            exroi1 = e.roi
-            out1 = avgHRF(e.expdesign.onsets_A, e.roi, e.expdesign.loadvolume.tr)
-            onsetA1 = d.onsets_A
-            print('regis 1')
-        elif uisi == 17 and lisi == 4:
-            exroi2 = e.roi
-            out2 = avgHRF(e.expdesign.onsets_A, e.roi, e.expdesign.loadvolume.tr)
-            onsetA2 = d.onsets_A
-            print('regis 2')
-        elif uisi == 17 and lisi == 7:
-            exroi3 = e.roi
-            out3 = avgHRF(e.expdesign.onsets_A, e.roi, e.expdesign.loadvolume.tr)
-            onsetA3 = d.onsets_A
-            print('regis 3')
-        elif uisi == 8 and lisi == 5:
-            exroi4 = e.roi
-            onsetA4 = d.onsets_A
-            print('regis 4')
-        elif uisi == 8 and lisi == 2:
-            exroi5 = e.roi
-            onsetA5 = d.onsets_A
-            print('regis 5')  
-        elif uisi == 19 and lisi == 11:
-            exroi6 = e.roi
-            onsetA6 = d.onsets_A
-            print('regis 6')
-        elif uisi == 3 and lisi == 2:
-            exroi7 = e.roi
-            onsetA7 = d.onsets_A
-            print('regis 7')
+        c1[k,l] = pca.explained_variance_[0]
+        c2[k,l] = pca.explained_variance_[1]
+        c3[k,l] = pca.explained_variance_[2]
+        if lisi == 15 and uisi == 17:
+            exroi1 = temp
+            print("1")
+        if lisi == 1 and uisi == 7:
+            exroi2 = temp
+            print("2")
+        if lisi == 7 and uisi == 18:
+            exroi3 = temp
+            print("3")
+        l += 1
+    k += 1
     print(lisi)
 
         
 #%%
 from tools import plotdata
-plotdata.plotdata(p1,a1,200,70)
+fig = plotdata.plotdata(c1,p2,200,70,normalize = True)
+#fig.savefig("Figures/Singletrial/st_det_heff.png",dpi = 600,bbox_inches = 'tight')
 #plotdata.plotdata(c1,c3,4500,4500)
 
 #%%
@@ -104,8 +90,11 @@ def avgHRF(onsets,brain,tr):
 
 #out = avgHRF(e.expdesign.onsets_A, e.roi, e.expdesign.loadvolume.tr)
 #%%
-from scipy.signal import savgol_filter as saf
-        
+for i in range(1,5):
+    for j in range(1,5):
+        if i>j:
+            continue
+        print(i,j)
         
         
         
--- tools/plotdata.py
+++ tools/plotdata.py
@@ -8,14 +8,20 @@ Created on Tue Apr 14 15:55:51 2020
 
 import matplotlib.pyplot as plt
 import numpy as np
-def plotdata(data1,data2,max1,max2):
+def plotdata(data1,data2,max1 ,max2 ,title1 = "Detection Power",
+             title2 = "HRF Estimation Efficiency",normalize = False):
+    if normalize:
+        data1 = data1/np.amax(data1)
+        data2 = data2/np.amax(data2)
+        max1 = 1
+        max2 = 1
     fig= plt.figure(figsize = (5,10))
     ax = fig.add_subplot(211)
     im = ax.imshow(data1,vmin = 0, vmax = max1,cmap = 'jet')
     ax.invert_yaxis()
     ax.set_xlabel("Upper Bound of ISI")
     ax.set_ylabel("Lower Bound of ISI")
-    ax.set_title('Detection Power')
+    ax.set_title(title1)
     fig.colorbar(im)
     labels = np.arange(1,len(data1)+1,2).astype(str)
     plt.xticks(np.arange(0,len(data1),2), labels)
@@ -27,7 +33,7 @@ def plotdata(data1,data2,max1,max2):
     ax.invert_yaxis()
     ax.set_xlabel("Upper Bound of ISI")
     ax.set_ylabel("Lower Bound of ISI")
-    ax.set_title('HRF Estimation Efficiency')
+    ax.set_title(title2)
     fig.colorbar(im2)
     plt.xticks(np.arange(0,len(data2),2), labels)
     plt.yticks(np.arange(0,len(data2),2), labels)

7dcead724de794b891df24ee6d33cc62d96063b8
soukhind2 <skndas@ucdavis.edu>
2020-04-23 18:24:46 -0700

    Some issues

--- scratch.py
+++ scratch.py
@@ -19,4 +19,40 @@ plt.legend()
 plt.title("Non linear effects",size = 20)
 labels = np.arange(18).astype(str)
 plt.xticks(np.arange(18), labels, size = 15)
+#%%
+x1 = np.zeros((19))
+x2 = np.zeros((19))
+for lisi in range(1,19):
+    d = design.expdesign(1, lisi, lisi, 0.1, 100, [102], lv)
+    data = d.tcourse()
+    e = design.expanalyse(data, np.array([1]), expdesign = d)
+    x1[lisi-1] = e.calc_Fd()
+    x2[lisi-1] = e.calc_Fe(ncond = 1)
+    print(lisi)
 
+#%%
+#%%
+from tools import plotdata
+plotdata.plotdata(p1,p2,150,3)
+#%%
+
+temp = e.roi.reshape(1,e.roi.shape[0])
+pca.fit(out)
+
+#%%
+def avgHRF(onsets,brain,tr):
+    res = 20
+    out = np.zeros((len(onsets),res))
+    for i in range(0,len(onsets)):
+        start = (onsets[i] / tr).astype('int')
+        series_A = np.linspace(start - 3,start + 16,res).astype('int')
+        if series_A[-1]  >= brain.shape[0]:
+            break
+        else:
+            out[i,:] = brain[series_A]
+        
+    return out
+
+#out = avgHRF(e.expdesign.onsets_A, e.roi, e.expdesign.loadvolume.tr)
+#%%
+from scipy.signal import savgol_filter as saf
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -5,7 +6,6 @@ Created on Thu Apr 16 23:38:21 2020
 
 @author: soukhind
 """
-
 import loadvolume
 
 lv = loadvolume.loadvolume('Participant_03_rest_run02.nii')
@@ -15,41 +15,41 @@ lv.loadmask()
 lv.generate_noise()
 lv.generate_region()
 
-#%%
+# %%
 
 import design
 import numpy as np
-from scipy.integrate import quad,simps
 from scipy.integrate import trapz
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 import importlib as imp
 import matplotlib.pyplot as plt
 pca = PCA(n_components = 10)
-p1 = np.zeros((19,19))
+p1 = np.zeros((38,38))
 
-p2 = np.zeros((19,19))
-a1 = np.zeros((19,19))
-c1 = np.zeros((19,19))
-c2 = np.zeros((19,19))
-c3 = np.zeros((19,19))
-
-for lisi in range(1,19):
-    for uisi in range(1,19):
+p2 = np.zeros((38,38))
+a1 = np.zeros((38,38))
+c1 = np.zeros((38,38))
+c2 = np.zeros((38,38))
+c3 = np.zeros((38,38))
+k =0
+l = 0
+for lisi in np.arange(1,19,0.5):
+    for uisi in np.arange(1,19,0.5):
         if lisi > uisi:
             continue
         d = design.expdesign(1, lisi, uisi, 0.1, 100, [102], lv)
         data = d.tcourse()
         e = design.expanalyse(data, np.array([1]), expdesign = d)
-        p1[lisi-1,uisi-1] = e.calc_Fd()
-        p2[lisi-1,uisi-1] = e.calc_Fe(ncond = 1)
+        p1[k,l] = e.calc_Fd()
+        p2[k,l] = e.calc_Fe(ncond = 1)
         m_data = e.roi/np.mean(e.roi)*100-100
-        a1[lisi-1,uisi-1] = trapz(m_data,dx = 1)
+        a1[k,l] = trapz(m_data,dx = 1)
         out = avgHRF(e.expdesign.onsets_A, m_data, e.expdesign.loadvolume.tr)
         pca.fit(out)
-        c1[lisi-1,uisi-1] = pca.explained_variance_[0]
-        c2[lisi-1,uisi-1] = pca.explained_variance_[1]
-        c3[lisi-1,uisi-1] = pca.explained_variance_[2]
+        c1[k,l] = pca.explained_variance_[0]
+        c2[k,l] = pca.explained_variance_[1]
+        c3[k,l] = pca.explained_variance_[2]
         if uisi == 14 and lisi == 2:
             exroi1 = e.roi
             out1 = avgHRF(e.expdesign.onsets_A, e.roi, e.expdesign.loadvolume.tr)
@@ -81,34 +81,12 @@ for lisi in range(1,19):
             exroi7 = e.roi
             onsetA7 = d.onsets_A
             print('regis 7')
+            l = l +1
+        k = k+1
     print(lisi)
 
         
-#%%
-from tools import plotdata
-plotdata.plotdata(p1,p2,150,3)
-#%%
 
-temp = e.roi.reshape(1,e.roi.shape[0])
-pca.fit(out)
-
-#%%
-def avgHRF(onsets,brain,tr):
-    res = 20
-    out = np.zeros((len(onsets),res))
-    for i in range(0,len(onsets)):
-        start = (onsets[i] / tr).astype('int')
-        series_A = np.linspace(start - 3,start + 16,res).astype('int')
-        if series_A[-1]  >= brain.shape[0]:
-            break
-        else:
-            out[i,:] = brain[series_A]
-        
-    return out
-
-#out = avgHRF(e.expdesign.onsets_A, e.roi, e.expdesign.loadvolume.tr)
-#%%
-from scipy.signal import savgol_filter as saf
         
         
         
